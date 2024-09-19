5f86687c2646ea79c6aab5b615bc32c9755e25cb
Yelyzaveta Skorohliad <skoroglyadliza@gmail.com>
2024-09-19 16:23:47 +0200

    Add js, vite and Update styles

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 a project to repeat all my skills in HTML and CSS.
+
+- I also decided to add some js technologies to this website to make all buttons work
+  So it's my first project I'm doing by myself with using some mechanics i've never used. (Vite, some js libraries)
---
+++ js/main.js
@@ -0,0 +1,25 @@
+// OPEN AND CLOSE MOBILE MENU FUNCTIONS
+
+const burgerBtn = document.querySelector(".nav-button-menu");
+const body = document.querySelector("#body");
+const menuBackdrop = document.querySelector(".mobile-menu-backdrop");
+
+function onBurgerBtnListener() {
+  menuBackdrop.classList.add("is-open");
+  body.classList.add("no-scroll");
+}
+
+burgerBtn.addEventListener("click", onBurgerBtnListener);
+
+const closeMenubtn = document.querySelector(".menu-close-button");
+
+function onCloseBtnListener() {
+  menuBackdrop.classList.remove("is-open");
+  body.classList.remove("no-scroll");
+}
+
+closeMenubtn.addEventListener("click", onCloseBtnListener);
+
+const menuNavigation = document.querySelector(".mobile-menu-navigation-list");
+
+menuNavigation.addEventListener("click", onCloseBtnListener);
